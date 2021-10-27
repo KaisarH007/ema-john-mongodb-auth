759c4e8b4c61e7a71cf4fec6001af08020a90609
KaisarH007 <kaisarh007@gamil.com>
2021-10-27 19:47:11 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+console.log(process.env);
+const firebaseConfig = {
+  apiKey: "AIzaSyAoc-pochv8DeMnN3_YJqAa27-rcWo247k",
+  authDomain: "ema-john-private-route.firebaseapp.com",
+  projectId: "ema-john-private-route",
+  storageBucket: "ema-john-private-route.appspot.com",
+  messagingSenderId: "313562027113",
+  appId: "1:313562027113:web:c69050361bf85a29c63ef8",
+};
+
+export default firebaseConfig;
